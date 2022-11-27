20d0b1fbe0264ffb336d47aba5f6a2b4f49651e4
jongsungbae <jongsung.jaybae@gmail.com>
2022-11-26 23:49:58 -0500

    create Maven Project

---
+++ RestAssured_EmployeeProject/src/main/java/RestAssured_EmployeeProject/RestAssured_EmployeeProject/App.java
@@ -0,0 +1,13 @@
+package RestAssured_EmployeeProject.RestAssured_EmployeeProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
